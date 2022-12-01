dae5ce0fd5a2a02748a77434d3b2b09c8a54c6b8
ItayHasidi <itayhasidi@gmail.com>
2022-12-01 18:35:03 +0200

    added initial functions
    need to add inputs, fix comments to TR, and linking to fairpy

---
+++ algorithms.py
@@ -0,0 +1,112 @@
+"""
+All these algorithms are based on the paper: Two-player fair division of indivisible items: Comparison of algorithms
+By: D. Marc Kilgour, Rudolf Vetschera
+
+programmers: Itay Hasidi & Amichai Bitan
+"""
+
+
+def sequential():
+    """
+    a.k.a OS
+    the algorithm receives:
+        z_a - partial allocation for player a.
+        z_b - partial allocation for player b.
+        u - all un-allocated objects.
+        l - the maximum rank of items currently considered for allocation
+    """
+    pass
+
+
+def restricted_simple():
+    """
+    a.k.a RS
+    the algorithm receives:
+        z_a - partial allocation for player a.
+        z_b - partial allocation for player b.
+        u - all un-allocated objects.
+        l - the maximum rank of items currently considered for allocation
+    """
+    pass
+
+
+def singles_doubles():
+    """
+    a.k.a SD
+    the algorithm receives:
+        z_a - partial allocation for player a.
+        z_b - partial allocation for player b.
+        u - all un-allocated objects.
+    """
+    pass
+
+
+def iterated_singles_doubles():
+    """
+    a.k.a IS
+    the algorithm receives:
+        z_a - partial allocation for player a.
+        z_b - partial allocation for player b.
+        u - all un-allocated objects.
+    """
+    pass
+
+
+def s1():
+    """
+    the algorithm receives:
+        z_a - partial allocation for player a.
+        z_b - partial allocation for player b.
+        u - all un-allocated objects.
+    """
+    pass
+
+
+def l1():
+    """
+    the algorithm receives:
+        z_a - partial allocation for player a.
+        z_b - partial allocation for player b.
+        u - all un-allocated objects.
+    """
+    pass
+
+
+def top_down():
+    """
+    a.k.a TD
+    u - all un-allocated objects.
+    """
+    pass
+
+
+def top_down_alternating():
+    """
+    a.k.a TA
+    u - all un-allocated objects.
+    """
+    pass
+
+
+def bottom_up():
+    """
+    a.k.a BU
+    u - all un-allocated objects.
+    """
+    pass
+
+
+def bottom_up_alternating():
+    """
+    a.k.a BA
+    u - all un-allocated objects.
+    """
+    pass
+
+
+def trump():
+    """
+    a.k.a TR
+    u - all un-allocated objects.
+    """
+    pass
